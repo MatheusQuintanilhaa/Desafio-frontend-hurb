6b3cb7535128b422f6bbb1b3a346660688c49d55
Matheus Quintanilha <matheus@matheus.com.br>
2024-04-25 12:46:14 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Clima App</h1>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
