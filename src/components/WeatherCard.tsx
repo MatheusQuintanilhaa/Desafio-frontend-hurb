7f68871859cd0ea0a6d1c9dcbdeb473083369ee4
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2024-06-11 18:00:37 -0300

    Finalizando a navbar no componente WeatherCard

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
-import "../App.css";
+import "./styles/WeatherCard.css";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Navbar from "react-bootstrap/Navbar";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 interface WeatherData {
   name: string;
   main: {
     temp: number;
+    humidity: number;
+    pressure: number;
+    feels_like: number;
   };
   weather: {
     description: string;
   }[];
+  wind: {
+    speed: number;
+  };
 }
 
 const WeatherCard: React.FC = () => {
@@ -23,13 +35,13 @@ const WeatherCard: React.FC = () => {
 
     try {
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=772920597e4ec8f00de8d376dfb3f094`
+        `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=c6df0b871b65037eff595b5de6efa912&units=metric`
       );
       const data = await response.json();
 
       setWeatherData(data);
     } catch (error) {
-      console.error("Erro ao buscaros dados do clima", error);
+      console.error("Erro ao buscar os dados do clima", error);
     }
   };
 
@@ -38,35 +50,45 @@ const WeatherCard: React.FC = () => {
   };
 
   return (
-    <div className="main">
-      <div
-        style={{
-          background:
-            weatherData && weatherData.main.temp > 25
-              ? "linear-gradient(to bottom, rgba(255,255,0,0.5), white)"
-              : weatherData && weatherData.main.temp < 20
-              ? "blue"
-              : "white",
-        }}
-      >
-        <input type="text" onChange={handleChangeLocation} />
-        <button onClick={handleSearch}>Buscar</button>
-        <div className="main__result">
-          {weatherData && (
-            <>
-              <div className="main__result-item">
-                <p>Cidade: {weatherData.name}</p>
-                <p>Temperatura: {weatherData.main.temp}</p>
-                <p>Tempo: {weatherData.weather[0].description}</p>
-              </div>
-            </>
-          )}
-          {/* previsao para os outros 7 dias */}
-          <div className="main__result-week">
-            <p>Terça feira</p>
+    <div className="weather-card">
+      <Navbar expand="lg" className="bg-body-tertiary fixed-top">
+        <Container fluid>
+          <Navbar.Brand href="#">Previsão do tempo</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Row className="w-100">
+              <Col xs={3}></Col>
+              <Col xs={6}>
+                <Form className="d-flex justify-content-center">
+                  <Form.Control
+                    type="text"
+                    placeholder="Digite a localização"
+                    className="me-2"
+                    aria-label="Search"
+                    onChange={handleChangeLocation}
+                  />
+                  <Button variant="outline-success" onClick={handleSearch}>
+                    Pesquisar
+                  </Button>
+                </Form>
+              </Col>
+              <Col xs={3}></Col>
+            </Row>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <section className="weather-info">
+        {weatherData && (
+          <div className="main__result-item">
+            <p>Localização: {weatherData.name}</p>
+            <p>Temperatura: {weatherData.main.temp} °C</p>
+            <p>Vento: {weatherData.wind.speed} m/s</p>
+            <p>Umidade: {weatherData.main.humidity} %</p>
+            <p>Pressão: {weatherData.main.pressure} hPa</p>
+            <p>Sensação Térmica: {weatherData.main.feels_like} °C</p>
           </div>
-        </div>
-      </div>
+        )}
+      </section>
     </div>
   );
 };
