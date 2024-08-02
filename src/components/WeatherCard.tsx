d10e276852421b7c629169fdce3067cc5ff81b0b
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2024-08-02 12:37:15 -0300

    Css weather card almost done

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -6,6 +6,7 @@ import Form from "react-bootstrap/Form";
 import Navbar from "react-bootstrap/Navbar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { FaSearch } from "react-icons/fa";
 
 interface WeatherData {
   name: string;
@@ -51,41 +52,51 @@ const WeatherCard: React.FC = () => {
 
   return (
     <div className="weather-card">
-      <Navbar expand="lg" className="bg-body-tertiary fixed-top">
-        <Container fluid>
-          <Navbar.Brand href="#">Previsão do tempo</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Row className="w-100">
-              <Col xs={3}></Col>
-              <Col xs={6}>
-                <Form className="d-flex justify-content-center">
-                  <Form.Control
-                    type="text"
-                    placeholder="Digite a localização"
-                    className="me-2"
-                    aria-label="Search"
-                    onChange={handleChangeLocation}
-                  />
-                  <Button variant="outline-success" onClick={handleSearch}>
-                    Pesquisar
-                  </Button>
-                </Form>
-              </Col>
-              <Col xs={3}></Col>
-            </Row>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <section className="card-nav">
+        <Navbar expand="lg" className="bg-dark navbar-dark fixed-top">
+          <Container fluid>
+            <Navbar.Brand href="#">Previsão do tempo</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Row className="w-100">
+                <Col xs={3}></Col>
+                <Col xs={6}>
+                  <Form className="d-flex justify-content-center">
+                    <Form.Control
+                      type="text"
+                      placeholder="Digite a localização"
+                      className="me-2 locationInput "
+                      aria-label="Search"
+                      onChange={handleChangeLocation}
+                    />
+                    <div className="searchInput">
+                      <FaSearch />
+                    </div>
+                    <Button variant="outline-light" onClick={handleSearch}>
+                      Pesquisar
+                    </Button>
+                  </Form>
+                </Col>
+                <Col xs={3}></Col>
+              </Row>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </section>
+
       <section className="weather-info">
         {weatherData && (
-          <div className="main__result-item">
-            <p>Localização: {weatherData.name}</p>
-            <p>Temperatura: {weatherData.main.temp} °C</p>
-            <p>Vento: {weatherData.wind.speed} m/s</p>
-            <p>Umidade: {weatherData.main.humidity} %</p>
-            <p>Pressão: {weatherData.main.pressure} hPa</p>
-            <p>Sensação Térmica: {weatherData.main.feels_like} °C</p>
+          <div className="info-result">
+            <div className="result-left">
+              <h1>Localização: {weatherData.name}</h1>
+            </div>
+            <div className="result-right">
+              <p>Temperatura: {weatherData.main.temp} °C</p>
+              <p>Umidade: {weatherData.main.humidity} %</p>
+              <p>Vento: {weatherData.wind.speed} m/s</p>
+              <p>Pressão: {weatherData.main.pressure} hPa</p>
+              <p>Sensação Térmica: {weatherData.main.feels_like} °C</p>
+            </div>
           </div>
         )}
       </section>
